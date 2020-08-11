0e8be694d481282e0a88a6316187c9ab47cea5a2
0honus0 <c397728390@gmail.com>
2020-08-11 22:51:40 +0800

    updata

--- change-variables.sh
+++ change-variables.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-sed 's/INPUTUSER/honus'              $GITHUB_WORKSPACE/1024.py -i
-sed 's/INPUTPASSWORD/Cds397728390'   $GITHUB_WORKSPACE/1024.py -i
-sed 's/INPUTSECRET/HBXP6TF6YDR6VNLL' $GITHUB_WORKSPACE/1024.py -i
+sed 's/INPUTUSER/${{ secrets.USER }}'              $GITHUB_WORKSPACE/1024.py -i
+sed 's/INPUTPASSWORD/${{ secrets.PASSWORD }}'   $GITHUB_WORKSPACE/1024.py -i
+sed 's/INPUTSECRET/${{ secrets.SECRET }}' $GITHUB_WORKSPACE/1024.py -i
