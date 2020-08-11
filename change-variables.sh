7fa070d30c35e7aea916b54009aa4d68279197d1
0honus0 <c397728390@gmail.com>
2020-08-11 22:56:07 +0800

    init

--- change-variables.sh
+++ change-variables.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-sed 's/INPUTUSER/${{ secrets.USER }}'              $GITHUB_WORKSPACE/1024.py -i
-sed 's/INPUTPASSWORD/${{ secrets.PASSWORD }}'   $GITHUB_WORKSPACE/1024.py -i
-sed 's/INPUTSECRET/${{ secrets.SECRET }}' $GITHUB_WORKSPACE/1024.py -i
+sed 's/INPUTUSER/$USER'           $GITHUB_WORKSPACE/1024.py -i
+sed 's/INPUTPASSWORD/$secrets.PASSWORD'   $GITHUB_WORKSPACE/1024.py -i
+sed 's/INPUTSECRET/$secrets.SECRET'       $GITHUB_WORKSPACE/1024.py -i
