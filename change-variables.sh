6c4c071d15eb184e64abe013ffa2b63d1c4bf626
honus <63273720+0honus0@users.noreply.github.com>
2020-08-11 23:42:07 +0800

    Update change-variables.sh

--- change-variables.sh
+++ change-variables.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-sed 's/INPUTUSER/$USER/'                   ./1024.py -i
-sed 's/INPUTPASSWORD/$PASSWORD/'   ./1024.py -i
-sed 's/INPUTSECRET/$SECRET/'       ./1024.py -i
+sed 's/INPUTUSER/$USER/' ./1024.py -i
+sed 's/INPUTPASSWORD/$PASSWORD/' ./1024.py -i
+sed 's/INPUTSECRET/$SECRET/' ./1024.py -i
