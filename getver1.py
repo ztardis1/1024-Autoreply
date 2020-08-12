a0668be5fb566314602ee2bed8b8fed95ec3a016
0honus0 <c397728390@gmail.com>
2020-08-12 22:00:01 +0800

    new vercode plat

---
+++ getver1.py
@@ -0,0 +1,29 @@
+# encoding:utf-8
+import requests
+import base64
+from PIL import Image
+import os
+import json
+
+class Getver:
+    def getcode(self):
+        im=Image.open('image.webp')
+        im.save('image.png')
+        f=open('image.png','rb')
+        image=base64.b64encode(f.read())
+        host='http://api.ttshitu.com/base64'
+        headers={
+            'Content-Type':'application/json;charset=UTF-8'
+        }
+        data={
+            'username': os.environ["username"] ,
+            'password': os.environ["password"] ,
+            'image':image.decode('utf-8')
+        }
+        res=requests.post(url=host,data=json.dumps(data))
+        res=res.text
+        m=res.find('result')
+        n=res.find('id')
+        res=res[m+9:n-3]
+        print(res)
+        return res
